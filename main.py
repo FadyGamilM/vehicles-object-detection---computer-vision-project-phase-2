0a2c8cb12d8bae74a0e9b586080222bf615a9183
fady <gamilfady605@gmail.com>
2022-05-23 02:37:49 +0200

    part 1

--- main.py
+++ main.py
@@ -1,107 +1,23 @@
+
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-import tensorflow as tf
-from tensorflow import keras
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-tf.test.is_built_with_cuda()
-print(tf.version.VERSION)
+
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+import queue
+import pickle
+import os
 import sys
-sys.version
-
-import tensorflow as tf
-print("Num GPUs Available", len(tf.config.experimental.list_physical_devices('GPU')))
-
-# Define a function to return some characteristics of the dataset 
-def data_look(car_list, notcar_list):
-    data_dict = {}
-    # Define a key in data_dict "n_cars" and store the number of car images
-    data_dict["n_cars"] = len(car_list)
-    # Define a key "n_notcars" and store the number of notcar images
-    data_dict["n_notcars"] = len(notcar_list)
-    # Read in a test image, either car or notcar
-    car_img = mpimg.imread(car_list[0])
-    # Define a key "image_shape" and store the test image shape 3-tuple
-    data_dict["image_shape"] = car_img.shape
-    # Define a key "data_type" and store the data type of the test image.
-    data_dict["data_type"] = car_img.dtype
-    # Return data_dict
-    return data_dict
-
-def visualize_images(input_images, num_cols, figure_name, cmap = None):
-    "Shows input images by stacking them in num_cols columns"
-    fig, axes = plt.subplots((int)((len(input_images) + 1) /num_cols), num_cols, figsize=(24, 20))
-
-    fig = plt.gcf()
-    fig.canvas.set_window_title(figure_name)
-    
-    print(figure_name)
-    
-    for ax, image in zip(axes.flat, input_images):
-        if(cmap == "gray" or cmap == 'hot'):
-            ax.imshow(image, cmap=cmap)
-        elif(image.shape[2]==1):
-            ax.imshow(image[:,:,0], cmap = cmap)
-        else:
-            ax.imshow(image, cmap=cmap)
-
-    plt.show()
-    
-
+from scipy.ndimage.measurements import label
 import glob
+from skimage.feature import hog
 
-#Read cars and not-cars images
-
-#Data folders
-vehicles_dir =     './vehicles/'
-non_vehicles_dir = './non-vehicles/'
-
-# images are divided up into vehicles and non-vehicles
-cars = []
-notcars = []
-
-# Read vehicle images
-images = glob.iglob(vehicles_dir + '/**/*.png', recursive=True)
-
-for image in images:
-        cars.append(image)
-        
-# Read non-vehicle images
-images = glob.iglob(non_vehicles_dir + '/**/*.png', recursive=True)
-
-for image in images:
-        notcars.append(image)
-    
-data_info = data_look(cars, notcars)
-
-print('Your function returned a count of', 
-      data_info["n_cars"], ' cars and', 
-      data_info["n_notcars"], ' non-cars')
-print('of size: ',data_info["image_shape"], ' and data type:', 
-      data_info["data_type"])
-
-#Visualize some input images
-
-import random
-num_images = 10
-
-# Just for fun choose random car / not-car indices and plot example images  
-cars_samples = random.sample(list(cars), num_images)
-notcar_samples = random.sample(list(notcars), num_images)
-
-# Read in car / not-car images
-car_images = []
-notcar_images = []
-for sample in cars_samples:
-
-    car_images.append(mpimg.imread(sample))
-for sample in notcar_samples:
-    notcar_images.append(mpimg.imread(sample))
-
-visualize_images(car_images, num_images, "Example Car images")
-visualize_images(notcar_images, num_images, "Example not-car images")
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
 
 
 def convert_color(img, conv='RGB2YCrCb'):
@@ -123,23 +39,7 @@ def convert_color(img, conv='RGB2YCrCb'):
     if conv == 'RGB2YUV':
         return cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
 
- # Plot the examples
-fig = plt.figure()
-plt.subplot(141)
-yuv_image = convert_color(car_images[1], 'RGB2YUV')
-plt.imshow(yuv_image[:,:,0], cmap ="gray")
-plt.title('Y channel')
-plt.subplot(142)
-plt.imshow(yuv_image[:,:,1], cmap ="gray")
-plt.title('U Channel')
-plt.subplot(143)
-plt.imshow(yuv_image[:,:,2], cmap ="gray")
-plt.title('V Channel')
-plt.subplot(144)
-plt.imshow(yuv_image)
-plt.title('YUV')
 
-from skimage.feature import hog
 
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
                         vis=False, feature_vec=True):
@@ -158,28 +58,169 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                        visualize=vis, feature_vector=feature_vec)
         return features
 
-orient = 9
-pix_per_cell = 8  #8*8
-cell_per_block = 2  #2*2
+  # While it could be heavy to include three color channels of a full resolution image,
+# you can perform spatial binning on an image and still retain enough information to help in finding vehicles.
 
-#notes:
-#get_hog_features() only accepts greyscale images 
-#else we will calculate hog for every channel R G B     
+# even going all the way down to 32 x 32 pixel resolution, 
+# the car itself is still clearly identifiable by eye, and this means that the relevant features are still preserved at this resolution.
 
-car_features, hog_image = get_hog_features(cv2.cvtColor(car_images[1], cv2.COLOR_RGB2GRAY), orient, pix_per_cell, cell_per_block, 
-                        vis=True, feature_vec=True)
+def bin_spatial(img, size=(16, 16)):         # to resize image combine each 4 pixel into 1 to make use of color info as a feature  
+    color1 = cv2.resize(img[:,:,0], size).ravel()
+    color2 = cv2.resize(img[:,:,1], size).ravel()
+    color3 = cv2.resize(img[:,:,2], size).ravel()
+    return np.hstack((color1, color2, color3))
 
-notcar_features, notcar_hog_image = get_hog_features(cv2.cvtColor(notcar_images[2], cv2.COLOR_RGB2GRAY), orient, pix_per_cell, cell_per_block, 
-                        vis=True, feature_vec=True)
 
-# Plot the examples
-fig = plt.figure()
-plt.subplot(131)
-plt.imshow(car_images[1])
-plt.title('Car Image')
-plt.subplot(132)
-plt.imshow(hog_image, cmap='gray')
-plt.title('car HOG')
-plt.subplot(133)
-plt.imshow(notcar_hog_image, cmap='gray')
-plt.title('not car HOG')
+# Define a function to compute color histogram features 
+    # Compute the histogram of the RGB channels separately
+    # Concatenate the histograms into a single feature vector
+
+# NEED TO CHANGE bins_range if reading .png files with mpimg!                                                                  32 is the width for 1 region in histogram
+def color_hist(img, nbins=32, bins_range=(0, 256)):              # to normalize histogram   1 Byte for pixel ranges [0-256]    256/32 = 8 --> 8 regions for histogram
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+
+#Read cars and not-cars images
+
+#Data folders
+test_images_dir = './test_images/'
+
+# images are divided up into vehicles and non-vehicles
+test_images = []
+
+images = glob.glob(test_images_dir + '*.jpg')
+
+for image in images:
+        test_images.append(mpimg.imread(image))
+
+# Define a single function that can extract features using hog sub-sampling and make predictions
+def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, vis_bboxes = False):
+    
+    draw_img = np.copy(img)
+    xstart = int(img.shape[1]/5) #from 1/5 of image 
+    xstop = img.shape[1]         #till end of image 
+    img_tosearch = img[ystart:ystop, xstart:xstop,:]  #crop image --> make it = size of rectangle
+    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YUV')
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1]/scale), np.int(imshape[0]/scale)))
+
+    ch1 = ctrans_tosearch[:,:,0]
+    ch2 = ctrans_tosearch[:,:,1]
+    ch3 = ctrans_tosearch[:,:,2]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
+    nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1 
+    nfeat_per_block = orient*cell_per_block**2
+    
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
+    cells_per_step = 2  # Instead of overlap, define how many cells to step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    rectangles = []
+
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            ypos = yb*cells_per_step
+            xpos = xb*cells_per_step
+            
+            # Extract HOG for this patch
+            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() # ravel flaten the array 
+            hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3)).reshape(1, -1)
+#             hog_features = np.hstack((hog_feat1))
+            
+            xleft = xpos*pix_per_cell
+            ytop = ypos*pix_per_cell
+
+            # Extract the image patch
+#             subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64,64))
+ 
+             # Get color features
+#             spatial_features = bin_spatial(subimg, size=spatial_size)
+#             hist_features = color_hist(subimg, nbins=hist_bins)
+            # Scale features and make a prediction
+#             stacked = np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1)
+            test_features = X_scaler.transform(hog_features)   
+            #test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))    
+            test_prediction = svc.predict(test_features)
+            
+            if test_prediction == 1 or vis_bboxes == True:
+                xbox_left = np.int(xleft*scale)
+                ytop_draw = np.int(ytop*scale)
+                win_draw = np.int(window*scale)
+                rectangles.append(((xbox_left+xstart, ytop_draw+ystart),(xbox_left+win_draw+xstart,ytop_draw+win_draw+ystart)))
+                              
+    return rectangles
+
+def get_rectangles(image, scales = [1, 1.5, 2, 2.5, 3], 
+                   ystarts = [400, 400, 450, 450, 460], 
+                   ystops = [528, 550, 620, 650, 700]):
+    out_rectangles = []
+    X_scaler = pickle.load(open('X_scaler.pkl', 'rb'))
+    svc = pickle.load(open('svc_pickle.pkl', 'rb'))
+    spatial_size = 32
+    hist_bins = 32
+    for scale, ystart, ystop in zip(scales, ystarts, ystops):    #zip [(1, 400, 528), (1.5, 400, 550), ...]
+        rectangles = find_cars(image, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins)
+        if len(rectangles) > 0:
+            out_rectangles.append(rectangles)
+    out_rectangles = [item for sublist in out_rectangles for item in sublist] 
+    return out_rectangles
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap
+
+
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    img_copy = np.copy(img)
+    result_rectangles = []
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        area = (bbox[1][1] - bbox[0][1]) * (bbox[1][0] - bbox[0][0])
+        if area > 40 * 40:
+            result_rectangles.append(bbox)
+            # Draw the box on the image
+            # cv2.rectangle(img_copy, bbox[0], bbox[1], (0,255,0), 6)    (bbox[0][0]-100,bbox[0][1]-100), (bbox[1][0]+100,bbox[1][1]+100)
+            cv2.rectangle(img_copy,(bbox[0][0]-int(0.02*bbox[0][0]),bbox[0][1]-int(0.02*bbox[0][1])), (bbox[1][0]+int(0.02*bbox[1][0]),bbox[1][1]+int(0.02*bbox[1][1])), (0,255,0), 6)
+    # Return the image
+    return result_rectangles, img_copy
+
+
+
